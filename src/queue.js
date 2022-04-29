2d2d209c07d96e8bfbf8958338b23c1b3c093ba4
Elem <elem15@yandex.ru>
2022-04-29 16:36:20 +0300

    feat: solve queue and stack

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
+class Queue {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // throw new NotImplementedError('Not implemented');
+    return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    // throw new NotImplementedError('Not implemented');
+    if (this.length === 0) {
+      this.head = new Node(value);
+    } else {
+      let current = this.head;
+
+      while(current.next) {
+        current = current.next;
+      }
+
+      current.next = new Node(value);
+    }
+
+    this.length++;   
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // throw new NotImplementedError('Not implemented');
+    const result = this.head.value;
+    this.head = this.head.next;
+    return result;
   }
 }
 
